33f2d07258396c270f6615991249f5836a93f738
SongChaeyoung <call0512@naver.com>
2026-04-28 10:56:30 +0900

    feat: Clarity 스크립트 코드 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 
 import { getThemeInitializationScript } from "@/features/toggle-theme/config/theme";
 import { ThemeToaster } from "@/features/toggle-theme/ui/theme-toaster";
@@ -86,6 +87,15 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{ __html: themeInitializationScript }}
         />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wii8yp6pkx");
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col">
         <ThemeToggle />
